b4d4fa467e1aa90c0136d86fbda2d2fc974ea399
alaa <jopa.samy2020@gmail.com>
2023-12-19 20:46:00 +0200

    07-Making the Custom Password Field Read-only

--- GoMarket/accounts/admin.py
+++ GoMarket/accounts/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
-# Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'last_login', 'date_joined', 'is_active')
+    list_display_links = ('email', 'username', 'first_name', 'last_name')   # اى ان عند الضغط عليه هيبقلك للتفاصيل 
+    readonly_fields = ('last_login', 'date_joined')     # غير قابل للتعديل وقابل للضغط
+    ordering = ('-date_joined',)     
+
+    # Bắt buộc phải khai báo
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
 
 
-admin.site.register(Account)
+admin.site.register(Account, AccountAdmin)
